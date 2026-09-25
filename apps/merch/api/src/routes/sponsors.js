00efa3c5b22e809e0ce28696eed22625c5bb8343
WHOLE DONUTS <thewholedonuts@gmail.com>
2026-09-25 19:01:49 -0400

    Update sponsors.js

--- apps/merch/api/src/routes/sponsors.js
+++ apps/merch/api/src/routes/sponsors.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { query, withTransaction } = require('../config/database');
