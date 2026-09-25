13e731e4a4969eb6502bca1a692a4392e6eb3fd8
WHOLE DONUTS <thewholedonuts@gmail.com>
2026-09-25 18:58:20 -0400

    Update referral.js

--- apps/merch/api/src/routes/referral.js
+++ apps/merch/api/src/routes/referral.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const { query } = require('../config/database');
 const { referralValidationLimiter } = require('../middleware/rateLimiter');
 const { hashIp, validateReferralCodeFormat } = require('../utils/referralCode');
