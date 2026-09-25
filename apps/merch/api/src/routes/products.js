09e3c5eebaaa3fd25a98d7abf019b9906dc32460
WHOLE DONUTS <thewholedonuts@gmail.com>
2026-09-25 18:57:55 -0400

    Update products.js

--- apps/merch/api/src/routes/products.js
+++ apps/merch/api/src/routes/products.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const { query } = require('../config/database');
 const { authenticateToken, requireAdmin } = require('../middleware/auth');
 
