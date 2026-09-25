75a5f09504a643dcd5f1b4055b4417028e42c55f
WHOLE DONUTS <thewholedonuts@gmail.com>
2026-09-25 19:44:04 -0400

    Update 002_deployment_readiness.sql

--- data/postgres/migrations/002_deployment_readiness.sql
+++ data/postgres/migrations/002_deployment_readiness.sql
@@ -1,4 +1,4 @@
-CREATE TABLE integration_events (
+ CREATE TABLE integration_events (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   provider VARCHAR(50) NOT NULL CHECK (provider IN ('shopify', 'printful')),
   delivery_id VARCHAR(255) NOT NULL,
