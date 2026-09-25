c76a647abe1eff16fd68c1383b34d6d49f87eb03
WHOLE DONUTS <thewholedonuts@gmail.com>
2026-09-25 18:55:40 -0400

    Update security.js

--- apps/merch/api/src/middleware/security.js
+++ apps/merch/api/src/middleware/security.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+ const crypto = require('crypto');
 const { isProduction } = require('../config/environment');
 
 const SAFE_METHODS = new Set(['GET', 'HEAD', 'OPTIONS']);
