217deb93b69c965ca5f5389fddf391bbd11d918a
WHOLE DONUTS <thewholedonuts@gmail.com>
2026-09-25 19:08:05 -0400

    Update referralStats.test.js

--- apps/merch/api/test/referralStats.test.js
+++ apps/merch/api/test/referralStats.test.js
@@ -1,4 +1,4 @@
-const assert = require('node:assert/strict');
+ const assert = require('node:assert/strict');
 const test = require('node:test');
 const referralRouter = require('../src/routes/referral');
 
