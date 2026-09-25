fb75ec57ede1cc1e3c2f811e758822429972db4b
WHOLE DONUTS <thewholedonuts@gmail.com>
2026-09-25 19:08:43 -0400

    Update shopifyWebhook.test.js

--- apps/merch/api/test/shopifyWebhook.test.js
+++ apps/merch/api/test/shopifyWebhook.test.js
@@ -1,4 +1,4 @@
-const assert = require('node:assert/strict');
+ const assert = require('node:assert/strict');
 const crypto = require('node:crypto');
 const test = require('node:test');
 const { verifyShopifyWebhook } = require('../src/services/shopifyWebhook');
